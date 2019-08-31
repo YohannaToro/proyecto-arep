25b52a4cae1f16aa4ad93d8168cca949d1c4d8d6
YohannaToro <yoha_duran156@hotmail.com>
2019-08-30 21:35:05 -0500

    delete deployment

--- src/main/java/edu/escuelaing/arem/project/app/AppServer.java
+++ src/main/java/edu/escuelaing/arem/project/app/AppServer.java
@@ -146,4 +146,10 @@ public class AppServer {
         }
 
     }
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
